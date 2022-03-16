57b413827453d6a4f90f7496d5799aaa4dfe8b2b
ไซยิดฮารูน อัลอิดรุส <syedymc@gmail.com>
2022-03-16 22:05:36 +0700

    prepare user

---
+++ backend/src/common/status.enum.js
@@ -0,0 +1,5 @@
+modules.export = {
+    ACTIVE: 'active',
+    INACTIVE: 'inactive',
+    DELETED: 'deleted'
+}
--- backend/src/main.js
+++ backend/src/main.js
@@ -1,11 +1,20 @@
 const express = require('express')
+const cors = require('cors')
 const app = express()
-const port = 8000
+const port = process.env.port || 8000
+
+app.use(express.urlencoded({extended: true}))
+app.use(express.json())
+app.use(cors())
+
+const userRouter = require('./modules/user/user.route')
+
+app.use('/user', userRouter)
 
 app.get('/', (req, res) => {
-  res.send('Hello World!')
+  res.send('user management service is online...')
 })
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
+  console.log(`user management service is online... [PORT:${port}]`)
 })
