7f25c1b1343c0feb1b9b51f97b143f1a8ea09937
ไซยิดฮารูน อัลอิดรุส <syedymc@gmail.com>
2022-03-17 09:32:18 +0700

    first data base set

---
+++ backend/src/modules/user/controllers/user.controller.js
@@ -0,0 +1,7 @@
+const userController = {
+    getUser (req, res){
+        res.send('get user success!')
+    }
+}
+
+module.exports = userController
--- backend/src/modules/user/user.route.js
+++ backend/src/modules/user/user.route.js
@@ -1,4 +1,7 @@
 const express = require('express')
+const userController = require('./controllers/user.controller')
 const router = express.Router()
+
+router.get('/',userController.getUser)
 
 module.exports = router
