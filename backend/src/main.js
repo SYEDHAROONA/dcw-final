a6fbc73d0a6b105088c52e06572b0206684a0b49
ไซยิดฮารูน อัลอิดรุส <syedymc@gmail.com>
2022-03-19 21:27:29 +0700

    setup database final

--- backend/src/main.js
+++ backend/src/main.js
@@ -3,7 +3,10 @@ const cors = require('cors')
 const mongoose = require('mongoose')
 const app = express()
 const port = process.env.port || 8000
-
+require('dotenv').config()
+const User = require('./user')
+mongoose.connect(process.env.DB_CONNECTION, {useNewUrlParser: true});
+console.log(process.env.DB_CONNECTION)
 app.use(express.urlencoded({extended: true}))
 app.use(express.json())
 app.use(cors())
@@ -15,6 +18,20 @@ app.use('/user', userRouter)
 app.get('/', (req, res) => {
   res.send('user management service is online...')
 })
+
+app.post('/testDB', (req, res) => {
+  const user = new User({
+    _id: new mongoose.Types.ObjectId,
+    name: req.body.name,
+    email: req.body.email
+  })
+  user.save().then(result => {
+    console.log(result)
+    res.send(result)
+  })
+  console.log('here')
+})
+
 
 app.listen(port, () => {
   console.log(`user management service is online... [PORT:${port}]`)
